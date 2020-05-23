6d3ffc97e0e51195789688b73905f685708c1d53
Fabiane Albuquerque <bii.nett@gmai.com>
2020-05-23 01:57:37 -0300

    Add cabybara configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
+require 'capybara/rails'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
