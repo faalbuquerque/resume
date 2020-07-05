d6062cefea56d562aaa7970efdbb905e30287143
Fabiane Albuquerque <bii.nett@gmail.com>
2020-07-04 21:23:14 -0300

    Create homepage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,4 @@
 
-RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
-end
-
-require 'capybara/rails'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -12,6 +6,14 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
+require 'capybara/rails'
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
