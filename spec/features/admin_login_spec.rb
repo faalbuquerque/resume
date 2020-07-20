722f1380bac7d5dd14a3036e93ad3cdbd117f61f
Fabiane Albuquerque <bii.nett@gmail.com>
2020-07-19 22:59:19 -0300

    Tests admin login

--- spec/features/admin_login_spec.rb
+++ spec/features/admin_login_spec.rb
@@ -1,15 +1,40 @@
 feature 'log in' do
-    context 'admin new login' do
-      it 'successfully' do
-        test = FactoryBot.create(:admin)
+  context 'new login' do
+    it 'successfully' do
+      clean_admin
+      test = FactoryBot.create(:admin)
 
-        visit new_admin_session_path
+      visit root_path
+      click_link I18n.t(:login).capitalize
 
-        fill_in I18n.t(:email).capitalize, :with => test.email
-        fill_in I18n.t(:password).capitalize, :with => test.password
-        click_button I18n.t(:login).capitalize
+      fill_in 'admin_email', :with => test.email
+      fill_in 'admin_password', :with => test.password
+      click_button 'Entrar'
 
-        expect(page).to have_content "Dashboard"
-      end
+      expect(page).to have_content "Dashboard"
+    end
+
+    it 'login failure or invalid' do
+      clean_admin
+      test = FactoryBot.create(:admin)
+
+      visit root_path
+      click_link I18n.t(:login).capitalize
+
+      fill_in 'admin_email', :with => test.email
+      fill_in 'admin_password', :with => test.password
+      test.delete
+      click_button 'Entrar'
+
+      expect(page).to have_content(I18n.t(:email).capitalize)
+      expect(page).to have_content(I18n.t(:password).capitalize)
+      expect(page).to have_button(I18n.t(:login).capitalize)
     end
   end
+end
+
+def clean_admin
+  if Admin.any?
+    Admin.destroy(Admin.first.id)
+  end
+end
--- spec/features/view_register_page_spec.rb
+++ spec/features/view_register_page_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 feature 'view register page' do
   context 'admin' do
     it 'successfully' do
-      visit new_admin_registration_path
+      visit root_path
+      click_link I18n.t(:login).capitalize
+      click_link I18n.t(:sign_up).capitalize
 
       expect(page).to have_content(I18n.t(:register_screen))
       expect(page).to have_content(I18n.t(:login).capitalize)
