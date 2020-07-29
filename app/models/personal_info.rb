e965986cf0ccb4083a77cb6e1159ed9e1bf6705b
Fabiane Albuquerque <bii.nett@gmail.com>
2020-07-29 04:01:28 -0300

    Fix errors rubocop

--- app/models/personal_info.rb
+++ app/models/personal_info.rb
@@ -1,10 +1,8 @@
 class PersonalInfo < ApplicationRecord
-
   validates :name, presence: true
   validates :address, presence: true
   validates :fone, presence: true
   validates :email, presence: true
   validates :links, presence: true
   validates :goals, presence: true
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "home#index"
-  resources :dashboard, only: [:index]
-  resources :personal_infos, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  root 'home#index'
+  resources :dashboard, only: %i[index]
+  resources :personal_infos, only: %i[index new create show
+                                      edit update destroy]
 end
