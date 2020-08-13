d5eb79ba30f0d144215e9a6cd85584b6d2520e2f
Fabiane Albuquerque <bii.nett@gmail.com>
2020-08-13 02:46:48 -0300

    Add test in blank of experiences

--- spec/features/experiences_edit_spec.rb
+++ spec/features/experiences_edit_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 feature 'experiences form' do
   context 'edit data' do
-
     it 'successfully' do
       clean_admin
       admin = FactoryBot.create(:admin)
@@ -32,6 +31,34 @@ feature 'experiences form' do
 
       expect(page).to have_content 'Designer'
       expect(page).to have_content 'Suporte'
+    end
+
+    it 'in blank' do
+      clean_admin
+      test = FactoryBot.create(:admin)
+      experience_test = FactoryBot.create(:experience)
+
+      visit experiences_path
+      
+      fill_in 'admin_email', with: test.email
+      fill_in 'admin_password', with: test.password
+      click_button I18n.t(:login).capitalize
+
+      find("a#experience_e_#{experience_test.id}").click
+
+      fill_in I18n.t(:occupation).capitalize, with: ''
+      fill_in I18n.t(:description_occupation).capitalize, with: ''
+      fill_in I18n.t(:company).capitalize, with: ''
+      fill_in I18n.t(:start_experience).capitalize, with: ''
+      fill_in I18n.t(:end_experience).capitalize, with: ''
+
+      click_button I18n.t(:save).capitalize
+
+      expect(page).to have_content("#{I18n.t(:occupation).capitalize} #{I18n.t('activerecord.errors.messages.blank')}")
+      expect(page).to have_content("#{I18n.t(:description_occupation).capitalize} #{I18n.t('activerecord.errors.messages.blank')}")
+      expect(page).to have_content("#{I18n.t(:company).capitalize} #{I18n.t('activerecord.errors.messages.blank')}")
+      expect(page).to have_content("#{I18n.t(:start_experience).capitalize} #{I18n.t('activerecord.errors.messages.blank')}")
+      expect(page).to have_content("#{I18n.t(:end_experience).capitalize} #{I18n.t('activerecord.errors.messages.blank')}")
     end
 
     it 'delete' do
