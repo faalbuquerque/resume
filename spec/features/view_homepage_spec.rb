e4beca2efef558045507b7c46117641d32dd0b1e
Fabiane Albuquerque <bii.nett@gmail.com>
2020-08-20 15:26:57 -0300

    Add link See more for Homepage items and tests

--- spec/features/view_homepage_spec.rb
+++ spec/features/view_homepage_spec.rb
@@ -6,6 +6,12 @@ feature 'view homepage' do
 
       expect(page).to have_content(I18n.t(:login).capitalize)
       expect(page).to have_content(I18n.t(:resume).capitalize)
+      expect(page).to have_content(I18n.t(:personal_infos))
+      expect(page).to have_content(I18n.t(:academic_infos))
+      expect(page).to have_content(I18n.t(:courses))
+      expect(page).to have_content(I18n.t(:skills))
+      expect(page).to have_content(I18n.t(:experiences))
+      expect(page).to have_content(I18n.t(:projects))
     end
   end
 end
