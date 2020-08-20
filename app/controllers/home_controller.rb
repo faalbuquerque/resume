f4f64a7934639397e0a66ca258d63007065e468c
Fabiane Albuquerque <bii.nett@gmail.com>
2020-08-20 14:28:07 -0300

    Add and Organize items in homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @personal_infos = PersonalInfo.all
+    @academic_infos = AcademicInfo.all
+    @courses = Course.all
+    @skills = Skill.all
+    @experiences = Experience.all
+    @projects = Project.all
   end
 end
